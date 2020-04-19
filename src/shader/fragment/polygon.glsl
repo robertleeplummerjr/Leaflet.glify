beb8334c8facf0b6287504255a8483ad9c804e04
Robert Plummer <robertleeplummerjr@gmail.com>
2020-04-19 07:46:49 -0400

    Updates

--- src/shader/fragment/polygon.glsl
+++
@@ -1,7 +0,0 @@
-precision mediump float;
-uniform float opacity;
-varying vec4 _color;
-
-void main() {
-  gl_FragColor = vec4(_color[0], _color[1], _color[2], opacity);
-}
--- src/shader/fragment/square.glsl
+++
@@ -1,8 +0,0 @@
-precision mediump float;
-varying vec4 _color;
-uniform float opacity;
-
-void main() {
-    //squares
-    gl_FragColor = vec4(_color[0], _color[1], _color[2], opacity);
-}
