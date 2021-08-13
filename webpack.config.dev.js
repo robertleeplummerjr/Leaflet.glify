339f96917263d1f2daf407d24f5b599b32a7d310
Robert Plummer <robertleeplummerjr@gmail.com>
2021-08-13 11:28:53 -0400

    fix: gh-pages build and publish fix

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const webpackConfigGhPages = require('./webpack.config.gh-pages');
 
 module.exports = {
-    entry: './example.ts',
+    ...webpackConfigGhPages,
     watch: true,
     devtool: 'inline-source-map',
     devServer: {
@@ -11,40 +11,6 @@ module.exports = {
         port: 9000
     },
     mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.html?$/,
-                use: 'raw-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(glsl|vs|fs|vert|frag)$/,
-                exclude: /node_modules/,
-                use: [
-                    'ts-shader-loader'
-                ]
-            }
-        ],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: 'index.html', to: 'index.html' },
-                // build/css so it aligns with the parcel script
-                { from: 'node_modules/leaflet/dist/leaflet.css', to: 'build/css/leaflet.css' },
-                { from: 'data/**', to: '' },
-            ],
-        }),
-    ],
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.html'],
-    },
     output: {
         filename: 'glify-browser.js',
         path: path.resolve(__dirname, '.dev-server'),
